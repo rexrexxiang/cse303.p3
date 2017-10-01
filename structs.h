ceb6323c2a22e764df7d34188ccd366537723da4
rexrexxiang <31937570+rexrexxiang@users.noreply.github.com>
2017-10-01 11:24:06 -0400

    Upload Code

---
+++ structs.h
@@ -0,0 +1,18 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+/*
+ *
+ * Define page/sector structures here as well as utility structures
+ * such as directory entries.
+ *
+ * Sectors/Pages are 512 bytes
+ * The filesystem is 4 megabytes in size.
+ * You will have 8K pages total.
+ *
+ */
+
+
+
+
+#endif
